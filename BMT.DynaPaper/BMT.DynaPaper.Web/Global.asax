fc339acd90a16ce6559db971c5df5fb64433d016
dougajmcdonald <dougajmcdonald@gmail.com>
2014-04-01 16:05:00 +0100

    Template project added with very simple application class to dynamically create a canvas based on some input params (hard coded atm)

---
+++ BMT.DynaPaper/BMT.DynaPaper.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BMT.DynaPaper.Web.MvcApplication" Language="C#" %>
---
+++ BMT.DynaPaper/BMT.DynaPaper.Web/Scripts/app/BMT.DynaPaper.ts
@@ -0,0 +1,57 @@
+﻿module BMT.DynaPaper {
+
+    export interface IApplicationConfig {
+        canvasId: string;
+        containerId: string;
+        text: string;
+        baseColour: string;
+        width: number;
+        height: number;
+        font: string;        
+    }
+
+    export class Application {
+
+        private canvasId: string;
+        private containerId: string;
+        private text: string;
+        private baseColour: string;
+        private width: number;
+        private height: number;
+        private font: string;
+        private $: JQueryStatic;
+
+        constructor($: JQueryStatic, config: IApplicationConfig) {
+
+            console.log("ctor");
+
+            this.text = config.text;
+            this.baseColour = config.baseColour;
+            this.width = config.width;
+            this.height = config.height;
+            this.font = config.font;
+            this.canvasId = config.canvasId;
+            this.containerId = config.containerId;
+            this.$ = $;
+
+            this.createCanvas();
+        }
+
+        public createCanvas(): void {
+
+            var canvasHtml = $('<canvas id=' + this.canvasId + '>');
+            var containerHtml = $('<div id=' + this.containerId + '/>');
+
+            $('body  div.jumbotron').after(containerHtml);
+            containerHtml.append(canvasHtml);
+
+            canvasHtml.width(this.width);
+            canvasHtml.height(this.height);
+
+            console.log("create canvas");
+
+        }
+
+    }
+
+} 
